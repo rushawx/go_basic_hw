2d397b7b8a2bf3fbc2b9eb786e721ec8695593f2
= <=>
2025-01-08 21:24:28 +0300

    hw

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,8 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	value := getUserInput()
+	fmt.Println(calculateOutput(value, "EUR", "RUB"))
+}
+
+func getUserInput() float64 {
+	var value float64
+	fmt.Println("Enter a number: ")
+	_, err := fmt.Scan(&value)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return value
+}
+
+func calculateOutput(value float64, sourceCurrency string, targetCurrency string) string {
 	var usdToEur float64 = 0.95
 	var usdToRub float64 = 100
 	eurToRub := usdToRub / usdToEur
+	fmt.Printf("from %s to %s\n", sourceCurrency, targetCurrency)
 	fmt.Printf("eurToRub: %.2f\n", eurToRub)
+	return fmt.Sprintf("%.2f", value*eurToRub)
 }
